55cb7764b955f48fd68221d4f24bf7110baf1eef
rthoma38 <rthoma38@emich.edu>
2025-02-28 23:03:08 -0500

    Delete test_prime.py

--- test_prime.py
+++
@@ -1,7 +0,0 @@
-# test_prime.py
-import pytest
-from prime import is_prime  # Assuming you have a function named is_prime in prime.py
-
-def test_is_prime():
-    assert is_prime(5) == True
-    assert is_prime(4) == False
